2fb0f83de8cc0a2621703cfad2ae08fec7fc4aa2
Sergio Rodriguez <103063371+sergiothinks2@users.noreply.github.com>
2024-11-01 21:45:28 -0500

    Initial Commit

---
+++ Recipe App/Recipe App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe App
+//
+//  Created by Sergio Rodriguez on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
