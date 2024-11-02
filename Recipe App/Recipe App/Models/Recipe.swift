e6cb16c97c6ad932dad7e8353aca76adf68048bb
Sergio Rodriguez <103063371+sergiothinks2@users.noreply.github.com>
2024-11-01 21:59:13 -0500

    Recipe data model

---
+++ Recipe App/Recipe App/Models/Recipe.swift
@@ -0,0 +1,33 @@
+//
+//  Recipe.swift
+//  Recipe App
+//
+//  Created by Sergio Rodriguez on 11/1/24.
+//
+
+import Foundation
+
+struct Recipe: Codable, Identifiable {
+    let id: String
+    let name: String
+    let cuisine: String
+    let photoURLSmall: String
+    let photoURLLarge: String
+    let sourceURL: String?
+    let youtubeURL: String?
+
+    enum CodingKeys: String, CodingKey {
+        case id = "uuid"
+        case name
+        case cuisine
+        case photoURLSmall = "photo_url_small"
+        case photoURLLarge = "photo_url_large"
+        case sourceURL = "source_url"
+        case youtubeURL = "youtube_url"
+    }
+}
+
+// Root response
+struct RecipesResponse: Codable {
+    let recipes: [Recipe]
+}
